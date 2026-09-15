c220d72330d7f93e813f70edb13a5d8378d46c3a
alisherk06082005-max <alisherk06082005@gmail.com>
2026-09-15 16:28:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run_flask, daemon=True).start()
+
+import json
+import sqlite3
+# ... и дальше весь остальной твой код
 import json
 import random
 import sqlite3
